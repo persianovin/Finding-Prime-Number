a337bec581389c181978543bf3deedcdc9318736
PERSIANOVIN <71163563+persianovin@users.noreply.github.com>
2021-04-10 16:10:53 +0430

    Create Normal Method.py

---
+++ Normal Method.py
@@ -0,0 +1,18 @@
+# Normal method
+
+def is_prime(number):
+    if number <= 1:
+        return False
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+    else:
+        return True
+
+
+a = int(input('Enter a number: '))
+
+if is_prime(a):
+    print(f'{a} is a prime number')
+else:
+    print(f'{a} is NOT a prime number')
